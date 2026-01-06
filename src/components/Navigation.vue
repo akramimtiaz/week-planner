32875f69a64ff5077394c126dca977b27fdaec62
Akram Imtiaz <akram.imtiaz10@gmail.com>
2026-01-06 16:06:07 +0800

    navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
-  import { Camera } from 'lucide-vue-next';
+  import { CheckCheck, House, SquareCheckBig, Calendar } from 'lucide-vue-next';
 </script>
 
 <template>
-    <header class="border border-red-500 w-[100%] flex flex-row justify-between items-center py-3 px-6 bg-white shadow-lg">
+    <header class="w-[100%] flex flex-row justify-between items-center py-3 px-6 bg-white shadow-lg">
         <div class="flex flex-row gap-2 items-center">
-            <Camera color="red" :size="32" />
-            Task Planner
+            <CheckCheck color="#a2d1f6" :size="32" />
+            <span class="text-2xl font-bold nav-title">Task Planner</span>
         </div>
 
-        <nav class="border border-blue-500 flex flex-row">
-            <ul class="flex flex-row gap-4">
-                <li>
-                    Home
+        <nav class="flex flex-row">
+            <ul class="flex flex-row gap-8">
+                <li class="flex flex-row gap-2 items-center">
+                    <House :size="16" />
+                    <span>Home</span>
                 </li>
-                <li>
-                    Tasks
+                <li class="flex flex-row gap-2 items-center">
+                    <SquareCheckBig :size="16" />
+                    <span>Tasks</span>
                 </li>
-                <li>
-                    Planner
+                <li class="flex flex-row gap-2 items-center">
+                    <Calendar :size="16" />
+                    <span>Planner</span>
                 </li>
             </ul>
         </nav>
     </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .nav-title {
+        color: #121929;
+    }
+</style>
